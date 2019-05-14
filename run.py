f624ef897c8132968fb66fabf0953c8662981b7c
evan <evan@futunn.com>
2019-05-15 00:06:19 +0800

    利用diferred重复启动爬虫

--- run.py
+++ run.py
@@ -8,13 +8,20 @@ from scrapy.utils.project import get_project_settings
 from scrapy.utils.log import configure_logging
 from tutorial.spiders.quotes_spider import QuotesSpider
 
+
+def start_spider(arg, setting):
+    runner = CrawlerRunner(setting)
+    d = runner.crawl(QuotesSpider)
+    # twisted deferred callback 必须有arg参数,否则不会回调
+    d.addBoth(start_spider, setting)
+    # d.addBoth(lambda _ : reactor.stop())
+    return arg
+
 if __name__ == '__main__':
     setting = get_project_settings()
     configure_logging(setting)
 
-    runner = CrawlerRunner(setting)
-    d = runner.crawl(QuotesSpider)
-    d.addBoth(lambda _: reactor.stop())
+    start_spider(0, setting)
 
     logging.info('start run')
     reactor.run()
